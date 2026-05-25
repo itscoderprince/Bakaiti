b8d299cd17fd0b3f05c52f84d9e5926d676f00e5
itscoderprince <itscoderprince@gmail.com>
2026-05-25 15:39:37 +0530

    feat: implement core authentication flow and layout components for chat application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
-const App = ({ children }) => {
-  return <div>{children}</div>;
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { checkAuthThunk } from "./features/auth/store/auth.thunks.js";
+import { Loader2 } from "lucide-react";
+import { Outlet } from "react-router-dom";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { isCheckingAuth } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(checkAuthThunk());
+  }, [dispatch]);
+
+  if (isCheckingAuth) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Loader2 className="h-10 w-10 animate-spin text-primary" />
+      </div>
+    );
+  }
+
+  return <Outlet />;
 };
 
 export default App;
